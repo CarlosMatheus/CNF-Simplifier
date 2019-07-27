4edfb894d8d3a46ccd9fa57d386e36e100d8d72d
Carlos Matheus Barros da Silva <carlosmatheusbs@gmail.com>
2019-07-27 00:37:19 -0300

    Initiate reading

---
+++ main.py
@@ -0,0 +1,6 @@
+import reader
+
+if __name__ == '__main__':
+    rd = reader.Reader()
+    print(rd.read('input00.txt'))
+
--- reader.py
+++ reader.py
@@ -1,4 +1,18 @@
+from os import path
+
 
 class Reader:
-    def __init__(self):
-        pass
+    def __init__(self, input_folder_name='inputs'):
+        self.input_folder_name = input_folder_name
+        self.this_file_path = path.dirname(path.abspath(__file__))
+        print(self.this_file_path)
+
+    def read(self, file_name):
+        file_path = path.join(path.join(self.this_file_path, self.input_folder_name), file_name)
+        print(file_path)
+        file = open(file_path, 'r').read()
+
+        return file
+
+
+
