4384c365c74a41576ae1d76513f2c9798b163e87
Carlos Matheus Barros da Silva <carlosmatheusbs@gmail.com>
2019-07-29 18:46:23 -0300

    Create the check subclause function

--- clause.py
+++ clause.py
@@ -1,4 +1,5 @@
 import utils
+from variable import Variable
 
 
 class Clause:
@@ -17,11 +18,38 @@ class Clause:
         :complexity: O(n) where n in the number of literals inside the clause
         :return: Clause
         """
-        new_variable_list = [lit.copy() for lit in self.variable_list] # todo: make copy function
+        new_variable_list = [lit.copy() for lit in self.variable_list]
         new_clause = Clause(new_variable_list)
         return new_clause
 
+    def is_sub_clause_of(self, other_clause):
+        """
+        checks wheater a clause is a subclause of the other one
+        :complexity: O(n) where n is the number of literal present on this class
+        :param other_clause: the other clause
+        :return: boolean
+        """
+        for lit in self.get_literals():
+            if not other_clause.has_literal(lit):
+                return False
+        return True
+
+    def has_literal(self, lit: Variable):
+        """
+        Checkes whether a literal objesct is present on a clause
+        :complexity: O(1)
+        :param lit: literal obejesct of class Variable
+        :return: boolean
+        """
+        return self.is_literal_value_present(lit.variable_value)
+
     def is_literal_value_present(self, literal_value: int):
+        """
+        checkes whether a literal value int is present on literal set
+        :complexity: O(1) -> set check uses a hashtable, worse case could be O(n)
+        :param literal_value: literal value to check
+        :return: boolean
+        """
         return literal_value in self.literals_set
 
     def get_literals(self):
