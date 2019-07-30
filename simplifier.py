d663c8f99f5f2998aff02cd8618a3cdf2b630d19
Carlos Matheus Barros da Silva <carlosmatheusbs@gmail.com>
2019-07-30 18:49:47 -0300

    Make complete simplification

--- simplifier.py
+++ simplifier.py
@@ -5,9 +5,18 @@ class Simplifier:
     def __init__(self):
         pass
 
-    def complete(self):
-        pass
+    def complete(self, cnf):
 
+        cnf = cnf.tautology_elimination()
+        cnf = cnf.hidden_tautology_elimination()
+        cnf = cnf.asymmetric_tautology_elimination()
 
+        cnf = cnf.blocked_clause_elimination()
+        cnf = cnf.hidden_blocked_clause_elimination()
+        cnf = cnf.asymmetric_tautology_elimination()
 
+        cnf = cnf.subsumption_elimination()
+        cnf = cnf.hidden_subsumption_elimination()
+        cnf = cnf.asymmetric_tautology_elimination()
 
+        return cnf
